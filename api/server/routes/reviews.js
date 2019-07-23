dc7a1743c2f2975cae11797e9cc9d1cb4053aaff
Louishancquart <hancquartlouis@gmail.com>
2019-07-23 22:46:36 +0530

    add possibility to filter by review or by videoURL

--- api/server/routes/reviews.js
+++ api/server/routes/reviews.js
@@ -19,21 +19,6 @@ module.exports = (fastify, opts, next) => {
     })
   })
 
-  // Get all reviews for a video
-  // fastify.get('/', { schema: opts.schemas.list }, async (req, reply) => {
-  //   Review.find({}, (err, reviews) => {
-  //     let reviewMap = {}
-  //
-  //     reviews.forEach((review) => {
-  //       if (!reviewMap[review.videoUrl]) reviewMap[review.videoUrl] = []
-  //       reviewMap[review.videoUrl].push(review)
-  //     })
-  //
-  //     reply.send(reviewMap)
-  //   })
-  // })
-
-
   // Get  reviews by videoUrl
   fastify.get('/:videoUrl', { schema: opts.schemas.get }, async (req, reply) => {
     if (!req.params.videoUrl) {
@@ -55,17 +40,17 @@ module.exports = (fastify, opts, next) => {
     })
   })
 
-  // Get a review by reviewId
-  fastify.get('/reviews/:reviewId', { schema: opts.schemas.get }, async (req, reply) => {
-    if (!req.params.reviewId) {
+  // Get a review by reviewId for a videoURL
+  fastify.get('/:videoUrl/:reviewId', { schema: opts.schemas.get }, async (req, reply) => {
+    if (!req.params.videoUrl) {
       req.log.info('redirect on reviews')
       reply.redirect(opts.prefix ? opts.prefix : '/')
       return
     }
 
-    req.log.info('request for a reiview : ' + req.params.reviewId)
+    req.log.info('request for a review  : ' + req.params.reviewId + ' on  a  videoURL' + req.params.videoUrl)
 
-    Review.find({reviewId: req.params._id}, (err, reviews) => {
+    Review.find({_id: req.params.reviewId}, (err, reviews) => {
       let reviewMap = {}
 
       reviews.forEach((review) => {
@@ -89,6 +74,21 @@ module.exports = (fastify, opts, next) => {
   //   let review = new Review(req.body.review)
   //   review.save((err) => {
   //     reply.send({ err: !!err })
+  //   })
+  // })
+
+
+  // Get all reviews for a video
+  // fastify.get('/', { schema: opts.schemas.list }, async (req, reply) => {
+  //   Review.find({}, (err, reviews) => {
+  //     let reviewMap = {}
+  //
+  //     reviews.forEach((review) => {
+  //       if (!reviewMap[review.videoUrl]) reviewMap[review.videoUrl] = []
+  //       reviewMap[review.videoUrl].push(review)
+  //     })
+  //
+  //     reply.send(reviewMap)
   //   })
   // })
 
